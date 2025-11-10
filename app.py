e4ffe29fafe76c454a28db3834398843b04b2ab4
Chobby12 <git add omenyitk@gmail.com>
2025-11-10 10:43:02 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,102 @@
+students = [
+    {
+        "name":"Patrick",
+        "age": 12,
+        "subjects": ["English", "Mathematics", "Computer Science", "Civic Education", "Basic Science"],
+        "characteristics": {
+            "height": 6,
+            "waist": 30,
+            "shirt":"L"
+        } 
+    },
+    {
+        "name":"Agnes",
+        "age": 14,
+        "subjects": ["Physics", "Chemistry", "Biology", "Literature"],
+        "characteristics": {
+            "height": 5.5,
+            "waist": 28,
+            "shirt":"Xl"
+        } 
+    },
+    {
+        "name":"Tobenna",
+        "age": 12,
+        "subjects": ["English", "Mathematics", "Computer Science", "Civic Education", "Basic Science"],
+        "characteristics": {
+            "height": 5.2,
+            "waist": 27,
+            "shirt":"M"
+        } 
+    },
+    {
+        "name":"Ade",
+        "age": 40,
+        "subjects": ["Anatomy", "Physiology", "Embroyology", "Histology", "Medical Biochemistry", "Pharmacology"],
+        "characteristics": {
+            "height": 5.8,
+            "waist": 40,
+            "shirt":"XXL"
+        } 
+    },
+    {
+        "name":"Uriel",
+        "age":17,
+        "subjects": ["English","Economics","CRS", "History", "Marketing", "Commerce"],
+        "characteristics": {
+            "height": 4.6,
+            "waist": 22,
+            "shirt":"S"
+        } 
+    },
+    {
+        "name":"Yomi",
+        "age": 10,
+        "subjects": ["English", "Mathematics", "Computer Science", "Civic Education", "Basic Science"],
+        "characteristics": {
+            "height": 4,
+            "waist": 20,
+            "shirt":"S"
+        } 
+    },
+    {
+        "name":"Tunde",
+        "age": 17,
+        "subjects": ["English", "Physics", "Chemistry", "Biology", "Mathematics"],
+        "characteristics": {
+            "height": 6.2,
+            "waist": 30,
+            "shirt":"L"
+        } 
+    },
+    {
+        "name":"Toma",
+        "age": 50,
+        "subjects": ["English", "African Literature", "Phonics", "Lexicography"],
+        "characteristics": {
+            "height": 5.3,
+            "waist": 32,
+            "shirt":"XL"
+        } 
+    },
+    {
+        "name":"Osondu",
+        "age": 13,
+        "subjects": ["English", "Mathematics", "Computer Science", "Civic Education", "Basic Science"],
+        "characteristics": {
+            "height": 6,
+            "waist": 22,
+            "shirt":"L"
+        } 
+    }
+]
+
+
+print(len(students))
+
+# sorted_students = sorted(students, key=lambda x: x["age"], reverse=True)
+
+# num = 1
+# for student in sorted_students:
+#     print(f"{num}. {student["name"]} : {student["age"]}")
+#     num += 1
